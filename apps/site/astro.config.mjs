29f3206359ab70e0dcfe4f1df0e07d1f5a2da7c0
QianCheng <51521689+hiqiancheng@users.noreply.github.com>
2026-05-26 09:21:19 +0800

    ci(site): deploy website to GitHub Pages (#252)

--- apps/site/astro.config.mjs
+++ apps/site/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://touchai-org.github.io/TouchAI/',
+	site: 'https://touch-ai.org/',
 	integrations: [
 		starlight({
 			title: 'TouchAI',
